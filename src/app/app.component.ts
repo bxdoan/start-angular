83b8f379dad1f3706b2594f6ef2cc8ea8b8e5d4a
Doan Bui <bxdoan93@gmail.com>
2024-02-26 17:06:45 +0700

    add more route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
